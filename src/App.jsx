a968ecdeae6ebd0ec56209ec6d00fe8b14909819
apisapii <mh2689537@gmail.com>
2026-07-08 23:01:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Dashboard } from './Dashboard'
+ 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Halaman Utama / Root (Menggunakan fragment kosong sebagai placeholder) */}
+        <Route path="/" element={<Dashboard></Dashboard>} />
+
+        {/* Nanti jika ada halaman baru, daftarkan di bawah ini. Contoh: */}
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
